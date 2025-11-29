f5331d532940ae0271c68330cf1cdd9defefda43
Tim Shand <tim@tshand.com>
2025-11-29 18:20:54 +1300

    chore: Added notes on known issues to root module file.

--- environments/azure/workloads/swa-wwwtshandcom/main.tf
+++ environments/azure/workloads/swa-wwwtshandcom/main.tf
@@ -5,6 +5,10 @@
 # Type: Azure Static Web App, Key Vault
 # Deployment: GitHub Actions
 
+# Know Issues:
+# - Intermitent 404 errors after creation. 
+#   - Docs: https://learn.microsoft.com/en-us/answers/questions/5573452/intermittent-404-response-on-static-web-apps-via-c
+
 # Use IaC Backend vending to generate 'prd' and 'dev' GitHub envrionments. 
 # Deploy workload with GitHub actions, manually selecting the environment, or defaulting to 'dev' for auto-triggered. 
 # Static Web App deployment token is stored in Azure Key Vault within the workloads Resource Group. 
