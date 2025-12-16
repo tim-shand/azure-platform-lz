b05e51ba4e37e8ece46704e2d6faed1633c49487
Tim Shand <tim@tshand.com>
2025-12-17 12:50:46 +1300

    feat: Added output for Management Group and Subscription associations.

--- platforms/azure/platform-landing-zone/outputs.tf
+++ platforms/azure/platform-landing-zone/outputs.tf
@@ -1,1 +1,11 @@
 # Outputs
+
+output "plz_governance_mg_root" {
+  description = "Name ID of the top-level management group."
+  value       = module.plz_governance.plz_governance_mg_root
+}
+
+output "plz_governance_mg" {
+  description = "List of management groups and associated subscriptions."
+  value       = module.plz_governance.plz_governance_mg
+}
