afe4b5e1e89565e56a0410b37d146ae7d8b4de38
Tim Shand <tim@tshand.com>
2026-01-24 20:47:14 +1300

    chore: Code structure tidy up, minor changes to format.

--- deployments/plz-governance/main.management-groups.tf
+++ deployments/plz-governance/main.management-groups.tf
@@ -16,7 +16,7 @@ module "management_groups" {
   global                   = var.global                                   # Global configuration. 
   naming_prefix            = module.naming_management_groups.full_name    # Provide a name prefix used for resource naming (mg-abc). 
   subscriptions            = data.azurerm_subscriptions.all.subscriptions # Pass in all subscriptions from data call. 
-  management_group_root    = var.management_group_root                    # Root: Top-level MG representign the organisation. 
+  management_group_root    = var.management_group_root                    # Root: Top-level MG representing the organisation. 
   management_groups_level1 = var.management_groups_level1                 # Level 1: Nested under root MG. 
   management_groups_level2 = var.management_groups_level2                 # Level 2: Nested under level 1 MGs. 
   management_groups_level3 = var.management_groups_level3                 # Level 3: Nested under level 2 MGs. Leave blank "{}" if not required. 
