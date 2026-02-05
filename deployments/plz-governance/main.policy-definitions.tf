0cb13d054949f783af314b1c9c480e960dfcc23b
Tim Shand <tim@tshand.com>
2026-02-05 23:38:21 +1300

    fix: Updated custom policy assignments to use correct naming and locals after refactor.

--- deployments/plz-governance/main.policy-definitions.tf
+++ deployments/plz-governance/main.policy-definitions.tf
@@ -1,14 +1,16 @@
-#=====================================================#
-# Platform LZ: Governance - Policy Definitions
-#=====================================================#
+#====================================================================================#
+# Governance: Policy Definitions
+# Description: 
+# - Create Policy Definitions from local JSON files. 
+#====================================================================================#
 
-# Loop each JSON file in directory and create Policy Definition. 
+# Policy: Definitions - Loop each JSON file in directory and create Policy Definition. 
 resource "azurerm_policy_definition" "custom" {
   for_each     = local.policies # Local map variable of policy names and content. 
   name         = each.key       # Use trimmed filename as policy definition name. 
   policy_type  = "Custom"
   mode         = each.value.mode
-  display_name = "[${upper(var.naming.stack_code)}] - ${each.value.displayName}"
+  display_name = "[${upper(var.stack.naming.workload_code)}] - ${each.value.displayName}"
   description  = try(each.value.description, null)          # Try if it exists, use it - otherwise use null.
   metadata     = jsonencode(try(each.value.metadata, {}))   # Try if it exists, use it - otherwise empty. 
   parameters   = jsonencode(try(each.value.parameters, {})) # Try if it exists, use it - otherwise empty. 
