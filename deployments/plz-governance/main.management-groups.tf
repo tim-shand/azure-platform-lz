ecce98f095f590d67daf09d09a1132ff1e874e72
Tim Shand <tim@tshand.com>
2026-02-08 15:40:50 +1300

    chore: Update MG module for new variable names..

--- deployments/plz-governance/main.management-groups.tf
+++ deployments/plz-governance/main.management-groups.tf
@@ -56,15 +56,3 @@ resource "azurerm_management_group" "level3" {
   subscription_ids           = lookup(local.management_group_subscriptions_level3, each.key, []) # Assign mapped subscriptions from locals.  
   parent_management_group_id = local.management_group_ids_level3[each.value.parent_mg_name]      # Assign to level 2 parent management group.
 }
-
-# module "management-groups" {
-#   source                   = "../../modules/gov-management-groups"
-#   subscriptions            = data.azurerm_subscriptions.all # Pass in all subscriptions. 
-#   naming_prefix            = var.global.naming.org_prefix   # "abc"
-#   management_group_core    = data.azurerm_management_group.core
-#   management_groups_level1 = var.management_groups_level1
-#   management_groups_level2 = var.management_groups_level2
-#   management_groups_level3 = var.management_groups_level3
-#   management_groups_level4 = {} # Leave blank to exclude this level. 
-#   management_groups_level5 = {} # Leave blank to exclude this level.
-# }
