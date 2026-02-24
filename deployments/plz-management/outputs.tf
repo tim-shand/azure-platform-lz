7442d7ec39f02bb67749c88e4d0514b1e4d6b6e7
Tim Shand <tim@tshand.com>
2026-02-25 09:31:02 +1300

    chore: Removed noisey unused outputs.

--- deployments/plz-management/outputs.tf
+++ deployments/plz-management/outputs.tf
@@ -1,25 +1,3 @@
 # MANAGEMENT: General
 # ------------------------------------------------------------- #
 
-# output "activity_log_alerts" {
-#   description = "Map of generated activity log alerts."
-#   value = {
-#     #for k, v in azurerm_monitor_activity_log_alert.all :
-#     for k, v in local.activity_log_alerts_merged :
-#     k => {
-#       name         = v.name
-#       category     = v.criteria[0].category
-#       action_group = v.action
-#     }
-#   }
-# }
-
-output "activity_log_alerts_merged" {
-  description = "Merged map of activity log alert rules."
-  value       = local.activity_log_alerts_merged
-}
-
-# output "test" {
-#   value = local.activity_log_alerts_action_groups
-# }
-
