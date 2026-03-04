941f56cca0c0e39b8a5aceafa9cdcb13d5b3c59d
Tim Shand <tim@tshand.com>
2026-03-04 19:59:14 +1300

    feat: Added MDfC settings to management stack.

--- deployments/plz-management/outputs.tf
+++ deployments/plz-management/outputs.tf
@@ -1,3 +1,12 @@
 # MANAGEMENT: General
 # ------------------------------------------------------------- #
 
+# Log Analytics Workspace
+output "mgt_law_name" {
+  description = "Log Analytics Workspace name."
+  value       = azurerm_log_analytics_workspace.mgt_logs.name
+}
+output "mgt_law_rg" {
+  description = "Log Analytics Workspace resource group."
+  value       = azurerm_log_analytics_workspace.mgt_logs.resource_group_name
+}
