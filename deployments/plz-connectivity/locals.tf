54cafee833ec60b4d159ca8f24975c41e9de1db8
Tim Shand <tim@tshand.com>
2026-03-23 20:43:00 +1300

    refactor: Move to using independant hub services over single variable.

--- deployments/plz-connectivity/locals.tf
+++ deployments/plz-connectivity/locals.tf
@@ -4,14 +4,3 @@
 locals {
   tags_merged = merge(var.global.tags, var.stack.tags) # Merge global tags with stack tags. 
 }
-
-# locals {
-#   hub_services_enabled = {
-#     for k, v in var.hub_services :
-#     k => {
-#       enabled = v.enabled
-#       subnet  = v.subnet
-#     }
-#     if v.enabled == true # Only select enabled subnets. 
-#   }
-# }
