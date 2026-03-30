584f49e5a7cfbb008c073975bf9c912af578e05e
Tim Shand <tim@tshand.com>
2026-03-30 22:19:45 +1300

    feat: Added resource specific names as outputs.

--- modules/global-resource-naming/outputs.tf
+++ modules/global-resource-naming/outputs.tf
@@ -25,12 +25,69 @@ output "compact_name_unique" {
   value       = local.compact_name_unique
 }
 
-output "storage_account_name" {
-  description = "Pre-determined name for Storage Account."
-  value       = "${local.compact_name_unique}sa"
+# RESOURCES -------------------------------------- #
+
+output "resource_group" {
+  description = "Full length name separated by dashes."
+  value       = "rg-${local.full_name}"
 }
 
-output "key_vault_name" {
+output "storage_account" {
+  description = "Pre-determined name for Storage Account."
+  value       = "sa${local.compact_name_unique}"
+}
+
+output "key_vault" {
   description = "Pre-determined name for Key Vault."
-  value       = "${local.compact_name_unique}kv"
+  value       = "kv-${local.compact_name_unique}"
+}
+
+output "virtual_network" {
+  description = "Full length name separated by dashes."
+  value       = "vnet-${local.full_name}"
+}
+
+output "subnet" {
+  description = "Full length name separated by dashes."
+  value       = "snet-${local.full_name}"
+}
+
+output "log_analytics_workspace" {
+  description = "Full length name separated by dashes."
+  value       = "law-${local.full_name}"
+}
+
+output "user_assigned_managed_identity" {
+  description = "Full length name separated by dashes."
+  value       = "uai-${local.full_name}"
+}
+
+output "azure_firewall" {
+  description = "Full length name separated by dashes."
+  value       = "afw-${local.full_name}"
+}
+
+output "azure_firewall_policy" {
+  description = "Full length name separated by dashes."
+  value       = "afwp-${local.full_name}"
+}
+
+output "bastion" {
+  description = "Full length name separated by dashes."
+  value       = "bas-${local.full_name}"
+}
+
+output "gateway_vpn" {
+  description = "Full length name separated by dashes."
+  value       = "vpng-${local.full_name}"
+}
+
+output "gateway_express_route" {
+  description = "Full length name separated by dashes."
+  value       = "ergw-${local.full_name}"
+}
+
+output "public_ip" {
+  description = "Full length name separated by dashes."
+  value       = "pip-${local.full_name}"
 }
