f71b9def96976334b11900d3a6753d260f38a852
Tim Shand <tim@tshand.com>
2026-04-24 14:04:59 +1200

    fix: Add import block for MDFC security settings.

---
+++ deployments/plz-management/imports.tf
@@ -0,0 +1,6 @@
+# Already set when MDFC is enabled previously - run import.
+import {
+  for_each = local.active_subscriptions
+  id       = "/subscriptions/${each.key}/providers/Microsoft.Security/workspaceSettings/default"
+  to       = azurerm_security_center_workspace.mgt[each.key]
+}
