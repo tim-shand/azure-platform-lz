32b34428808021f31b43ec5b3e22ac82310d825e
Tim Shand <tim@tshand.com>
2026-04-30 22:33:59 +1200

    fix: Update management to subscription assignment locals.

--- deployments/plz-governance/outputs.tf
+++ deployments/plz-governance/outputs.tf
@@ -35,26 +35,27 @@ output "policy_initiatives" {
   }
 }
 
-# output "policy_initiative_assignments" {
-#   description = "Map of policy initiative assignments to Management Groups."
-#   value = {
-#     for k, v in azurerm_management_group_policy_assignment.custom :
-#     k => {
-#       name                = v.name
-#       display_name        = v.display_name
-#       enforce             = v.enforce
-#       identity            = v.identity
-#       management_group_id = v.management_group_id
-#     }
-#   }
-# }
+output "policy_initiative_assignments" {
+  description = "Map of policy initiative assignments to Management Groups."
+  value = {
+    for k, v in azurerm_management_group_policy_assignment.custom :
+    k => {
+      name                = v.name
+      display_name        = v.display_name
+      enforce             = v.enforce
+      identity            = v.identity
+      management_group_id = v.management_group_id
+      parameters          = try(v.parameters, null)
+    }
+  }
+}
 
-# output "policy_managed_identity" {
-#   description = "Map of policy managed identity resource."
-#   value = {
-#     principal_id        = azurerm_user_assigned_identity.policy.principal_id
-#     name                = azurerm_user_assigned_identity.policy.name
-#     resource_group_name = azurerm_user_assigned_identity.policy.resource_group_name
-#     location            = azurerm_user_assigned_identity.policy.location
-#   }
-# }
+output "policy_managed_identity" {
+  description = "Map of policy managed identity resource."
+  value = {
+    principal_id        = azurerm_user_assigned_identity.policy.principal_id
+    name                = azurerm_user_assigned_identity.policy.name
+    resource_group_name = azurerm_user_assigned_identity.policy.resource_group_name
+    location            = azurerm_user_assigned_identity.policy.location
+  }
+}
