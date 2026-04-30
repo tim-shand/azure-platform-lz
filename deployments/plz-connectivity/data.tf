aa4e3db91bd6be929ea0a4ced891849250f3d6dd
Tim Shand <tim@tshand.com>
2026-05-01 10:24:47 +1200

    refactor: Updated design for connectivity stack components.

--- deployments/plz-connectivity/data.tf
+++ deployments/plz-connectivity/data.tf
@@ -1,40 +1,31 @@
-# GLOBAL / SHARED SERVICES
-# ------------------------------------------------------------- #
+# GENERAL ------------------------------------------------------------------ #
 
-# IaC: Get IaC subscription for aliased provider. 
-data "azurerm_subscription" "iac_sub" {
-  subscription_id = var.subscription_id_iac # Pass in the IaC subscription variable. 
+data "azuread_client_config" "current" {} # Get current user session data.
+data "azurerm_subscription" "current" {}  # Get current Azure subscription.
+data "azurerm_subscriptions" "all" {}     # Collect all available subscriptions.
+
+# REMOTE STATE ------------------------------------------------------------- #
+
+data "terraform_remote_state" "iac" {
+  backend = "azurerm"
+  config = {
+    resource_group_name  = var.remote_state_resource_group
+    storage_account_name = var.remote_state_storage_account
+    container_name       = "tfstate-${var.deployment_stacks.bootstrap.stack_name}"
+    key                  = "${var.deployment_stacks.bootstrap.stack_name}.tfstate"
+    use_azuread_auth     = true # Force Entra ID for authorisation over Shared Access Keys.
+  }
 }
 
-# Shared Services: Get App Configuration data using alias provider. 
-data "azurerm_app_configuration" "iac" {
-  provider            = azurerm.iac             # Use aliased provider to access IaC subscription. 
-  name                = var.global_outputs_name # Pass in shared services App Configuration name via workflow variable. 
-  resource_group_name = var.global_outputs_rg   # Pass in shared services App Configuration Resource Group name via workflow variable. 
+data "terraform_remote_state" "mgt" {
+  backend = "azurerm"
+  config = {
+    resource_group_name  = var.remote_state_resource_group
+    storage_account_name = var.remote_state_storage_account
+    container_name       = "tfstate-${var.deployment_stacks.management.stack_name}"
+    key                  = "${var.deployment_stacks.management.stack_name}.tfstate"
+    use_azuread_auth     = true # Force Entra ID for authorisation over Shared Access Keys.
+  }
 }
 
-# CONNECTIVITY: Hub VNet
-# ------------------------------------------------------------- #
-
-data "azurerm_app_configuration_key" "log_sa_id" {
-  configuration_store_id = data.azurerm_app_configuration.iac.id
-  key                    = var.global_outputs.management.storage_account_id # Refer to variable in globals.
-  label                  = var.global_outputs.management.label              # Related label used to identify entries.
-}
-
-# Log Analytics
-data "azurerm_app_configuration_key" "log_law_name" {
-  configuration_store_id = data.azurerm_app_configuration.iac.id
-  key                    = var.global_outputs.management.log_analytics_workspace_name # Refer to variable in globals.
-  label                  = var.global_outputs.management.label                        # Related label used to identify entries.
-}
-data "azurerm_app_configuration_key" "log_law_rg" {
-  configuration_store_id = data.azurerm_app_configuration.iac.id
-  key                    = var.global_outputs.management.log_analytics_workspace_resource_group # Refer to variable in globals.
-  label                  = var.global_outputs.management.label                                  # Related label used to identify entries.
-}
-data "azurerm_log_analytics_workspace" "log_law" {
-  name                = data.azurerm_app_configuration_key.log_law_name.value
-  resource_group_name = data.azurerm_app_configuration_key.log_law_rg.value
-}
-
+# STACK -------------------------------------------------------------------- #
--- deployments/plz-connectivity/main.hub-vnet.tf
+++ deployments/plz-connectivity/main.hub-vnet.tf
@@ -2,6 +2,7 @@
 # Connectivity: Hub VNet
 # Description: 
 # - Create VNet for centralized hub connectivity.
+# - Deploy Network Watcher and VNet flow logs to Log Analytics.
 #====================================================================================#
 
 # VNet: Hub
@@ -11,4 +12,35 @@ resource "azurerm_virtual_network" "hub" {
   location            = azurerm_resource_group.con.location
   tags                = local.tags_merged
   address_space       = var.vnet_hub_cidr
+}
+
+# Network Watcher: Main
+resource "azurerm_network_watcher" "hub" {
+  name                = "${module.naming_con.full_name}-nww"
+  resource_group_name = azurerm_resource_group.con.name
+  location            = azurerm_resource_group.con.location
+  tags                = local.tags_merged
+}
+
+# Network Watcher: Flow Log
+resource "azurerm_network_watcher_flow_log" "hub" {
+  name                 = "${module.naming_con.full_name}-nfl"
+  network_watcher_name = azurerm_network_watcher.hub.name
+  resource_group_name  = azurerm_resource_group.con.name
+  location             = azurerm_resource_group.con.location
+  tags                 = local.tags_merged
+  enabled              = true
+  target_resource_id   = azurerm_virtual_network.hub.id                     # ID of the hub VNet. 
+  storage_account_id   = data.azurerm_app_configuration_key.log_sa_id.value # Storage Account in Management stack (required). 
+  retention_policy {
+    enabled = true
+    days    = 30
+  }
+  traffic_analytics {
+    enabled               = true
+    workspace_id          = data.azurerm_log_analytics_workspace.log_law.workspace_id
+    workspace_region      = data.azurerm_log_analytics_workspace.log_law.location
+    workspace_resource_id = data.azurerm_log_analytics_workspace.log_law.id
+    interval_in_minutes   = 10
+  }
 }
