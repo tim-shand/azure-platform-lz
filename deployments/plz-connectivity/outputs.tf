3b31df313e5621b99e7653cf0ccd78592d35e71e
Tim Shand <tim@tshand.com>
2026-05-10 12:33:39 +1200

    feat: Added output for hub vnet properties.

--- deployments/plz-connectivity/outputs.tf
+++ deployments/plz-connectivity/outputs.tf
@@ -7,3 +7,12 @@
 #   description = "Output of the hub firewall management IP configuration."
 #   value       = azurerm_firewall.hub[0].ip_configuration
 # }
+
+output "hub_vnet" {
+    description = "Map of hub Virtual Network properties."
+    value = {
+        id = azurerm_virtual_network.hub.id
+        name = azurerm_virtual_network.hub.name
+        resource_group = azurerm_virtual_network.hub.resource_group
+    }
+}
