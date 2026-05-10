db6ef65e57cb118087f764abc6e178a60bbdc01c
Tim Shand <tim@tshand.com>
2026-05-10 13:14:52 +1200

    chore: Add outputs for Azure Firewall resources.

--- deployments/plz-connectivity/outputs.tf
+++ deployments/plz-connectivity/outputs.tf
@@ -1,13 +1,3 @@
-# output "hub_fwl_ips" {
-#   description = "Output of the hub firewall IP configuration."
-#   value       = azurerm_firewall.hub[0].ip_configuration
-# }
-
-# output "hub_fwl_ips_mgt" {
-#   description = "Output of the hub firewall management IP configuration."
-#   value       = azurerm_firewall.hub[0].ip_configuration
-# }
-
 output "hub_vnet" {
     description = "Map of hub Virtual Network properties."
     value = {
@@ -15,4 +5,18 @@ output "hub_vnet" {
         name = azurerm_virtual_network.hub.name
         resource_group = azurerm_virtual_network.hub.resource_group_name
     }
+}
+
+output "hub_firewall" {
+    description = "Map of Azure Firewall properties."
+    value = var.hub_firewall.enabled ? {
+        id              = azurerm_firewall.hub[0].id
+        name            = azurerm_firewall.hub[0].name
+        resource_group  = azurerm_firewall.hub[0].resource_group_name
+        policy_name     = azurerm_firewall_policy.hub[0].name
+        public_ip       = azurerm_public_ip.fw[0].id
+        public_ip_mgt   = azurerm_public_ip.fw_mgt[0].id
+        subnet          = azurerm_subnet.fw[0].address_prefixes
+        subnet_mgt      = azurerm_subnet.fw_mgt[0].address_prefixes
+    } : {}
 }
